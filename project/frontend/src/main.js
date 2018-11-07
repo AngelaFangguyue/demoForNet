e0885472bba975755ab45438f83531e1eb1f692f
unknown <hzyuqiubin@corp.netease.com>
2018-11-07 17:19:56 +0800

    add url-search-params-polyfill

--- project/frontend/src/main.js
+++ project/frontend/src/main.js
@@ -3,8 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/base.css';
+import axios from 'axios'
+import "url-search-params-polyfill"
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+
+Vue.prototype.$http = axios
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,5 +20,15 @@ new Vue({
   router,
   template: '<App/>',
   components: { App }
+})
+
+// axios的拦截器
+axios.interceptors.response.use(function(response){
+    return response
+}, function(error){
+    if(error.response && error.response.data && error.response.data === 'LOGIN REQUIRED'){
+        window.location.href = "/login"
+    }
+    return Promise.reject(error)
 })
 
